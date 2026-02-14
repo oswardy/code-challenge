82913674d454bf25b945abf654c0c64d47271af1
Osward Yong <59945925+oswardy@users.noreply.github.com>
2026-02-14 23:11:10 +0800

    Add files via upload

---
+++ code-challenge/problem1/solution.js
@@ -0,0 +1,31 @@
+//Assumption: If n <= 0, return 0
+
+var sum_to_n_a = function(n) {
+    if (n <= 0) return 0;
+
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
+};
+
+var sum_to_n_b = function(n) {
+    if (n <= 0) return 0;
+
+    return (n * (n + 1)) / 2;
+};
+
+var sum_to_n_c = function(n) {
+    if (n <= 0) return 0;
+
+    if (n===1) return 1;
+
+    return n + sum_to_n_c(n-1); 
+};
+
+console.log(sum_to_n_a(5));
+console.log(sum_to_n_b(2));
+console.log(sum_to_n_c(3));
